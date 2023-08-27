c6341774c2d4f8b29b9cfa14a7ae4ab565c03591
Karl Palsson <karlp@etactica.com>
2023-08-27 13:20:56 +0000

    refactor to a class, allow bt writes to screen

--- kble-ser1.py
+++ kble-ser1.py
@@ -26,71 +26,74 @@ k_char_2 = aioble.Characteristic(k_service, bluetooth.UUID("ec4ad188-b475-4461-a
 k_char_2_ddesc = aioble.Descriptor(k_char_2, bluetooth.UUID(0x2901), read=True)
 k_char_2_ddesc.write("my second char")
 
-aioble.register_services(temp_service)
-aioble.register_services(k_service)
 
 lolq = []
 
-async def handle_write_ctrl(dat):
-    while True:
-        await k_char_ctrl.written()
-        msg = k_char_ctrl.read()
-        print(f"received ctrl write", msg)
+
+class KApp:
+    def __init__(self):
+        aioble.register_services(k_service, temp_service)
+        self.tft = tft_config.config(rotation=3)
+        self.tft.init()
+        self.h = self.tft.height()
+        self.w = self.tft.width()
+
+    async def start(self, blah):
+        self.tft.on()
+
+        await asyncio.gather(
+            self.t_periph(),
+            self.t_screen_play(),
+        )
 
 
-async def handle_write2(dat):
-    while True:
-        await k_char_2.written()
-        msg = k_char_2.read()
-        print(f"received second write:", msg)
+    async def t_screen_play(self):
+        self.tft.text(font, "hello", 0, 0)
+        i = 0
+        while True:
+            await asyncio.sleep_ms(500)
+            i += 1
+            self.tft.text(font, f"hello {i}", 0, 0)
 
+    async def handle_write_ctrl(self):
+        while True:
+            await k_char_ctrl.written()
+            msg = k_char_ctrl.read()
+            print(f"received ctrl write", msg)
 
-# This is a single server object, I'd really like to make a multi conn one though really.
-async def k_periph_task():
-    asyncio.create_task(handle_write_ctrl("blah"))
-    asyncio.create_task(handle_write2("wop"))
+    async def handle_write2(self):
+        while True:
+            await k_char_2.written()
+            msg = k_char_2.read()
+            print(f"received second write:", msg)
+            # TODO - auto scroll text?! blah... screens are boring...
+            self.tft.text(font, msg, 0, 20)
 
-    while True:
-        async with await aioble.advertise(
-            _ADV_INTERVAL_MS,
-            name="temp-sense",
-            services=[_ENV_SENSE_UUID],
-            appearance=_GENERIC_THERMOMETER,
-            manufacturer=(0x999, b"1234"),
-        ) as connection:
-            print("Connection from", connection.device)
-            #await asyncio.create_task(t_handle_client(connection))
-            #await t_handle_client(connection)
-            await connection.disconnected()
-            print("disconnected, advertising again...")
+    # This is a single server object, I'd really like to make a multi conn one though really.
+    async def t_periph(self):
+        asyncio.create_task(self.handle_write_ctrl())
+        asyncio.create_task(self.handle_write2())
 
-
-async def t_screen_play():
-    tft = tft_config.config(rotation=3)
-    tft.init()
-    tft.on()
-    h = tft.height()
-    w = tft.width()
-
-    tft.text(font, "hello", 0, 0)
-
-    i = 0
-    while True:
-        await asyncio.sleep_ms(500)
-        i += 1
-        tft.text(font, f"hello {i}", 0, 0)
-
-
-
+        while True:
+            async with await aioble.advertise(
+                    _ADV_INTERVAL_MS,
+                    name="temp-sense",
+                    services=[_ENV_SENSE_UUID],
+                    appearance=_GENERIC_THERMOMETER,
+                    manufacturer=(0x999, b"1234"),
+            ) as connection:
+                print("Connection from", connection.device)
+                #await asyncio.create_task(t_handle_client(connection))
+                #await t_handle_client(connection)
+                await connection.disconnected()
+                print("disconnected, advertising again...")
 
 
 # Run both tasks.
 async def main():
     print("starting...")
-    await asyncio.gather(
-        k_periph_task(),
-        t_screen_play(),
-        )
+    app = KApp()
+    await app.start("adsf")
     print("never hit here")
 
 
